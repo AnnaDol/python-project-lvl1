808d699012e634aa0bf329087617ab0cd200360a
AnnaDol <annadolmatova01@gmail.com>
2021-02-24 12:47:38 -0500

    added github_actions

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,11 @@
 import prompt
+
+
 name = prompt.string('May I have your name? ')
+
+
 def welcome_user(name):
-	print('Hello, {}!'.format(name))
+    print('Hello, {}!'.format(name))
+
+
 welcome_user(name)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import prompt
-def welcome_user(name):
-	print('Hello, {}!'.format(name))
-def hello(who):
-	print("Welcome to the {}!".format(who))
-def main():
-	hello('Brain Games')
-	name = prompt.string('May I have your name? ')
-	welcome_user(name)
-	
-if __name__ == "__main__":
-	main()
 
+
+def welcome_user(name):
+    print('Hello, {}!'.format(name))
+
+
+def hello(who):
+    print("Welcome to the {}!".format(who))
+
+
+def main():
+    hello('Brain Games')
+    name = prompt.string('May I have your name? ')
+    welcome_user(name)
+
+
+if __name__ == "__main__":
+    main()
