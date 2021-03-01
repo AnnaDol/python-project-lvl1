54a0052f99aba9f7aaaf3b781e32bdabf35288d7
AnnaDol <annadolmatova01@gmail.com>
2021-03-01 12:25:17 -0500

    brain_even added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/AnnaDol/python-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/AnnaDol/python-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 [![Build Status](https://travis-ci.com/AnnaDol/python-project-lvl1.svg?branch=main)](https://travis-ci.com/AnnaDol/python-project-lvl1)
+[![asciicast](https://asciinema.org/a/GhOVsH6OrMchGDuXBc4PlNJd6.svg)](https://asciinema.org/a/GhOVsH6OrMchGDuXBc4PlNJd6)
+[![asciicast](https://asciinema.org/a/N7RJrIU4Yj30lRAdKAO9fEMlD.svg)](https://asciinema.org/a/N7RJrIU4Yj30lRAdKAO9fEMlD)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,58 @@
+import prompt
+from random import randint
+
+
+def welcome_user(name):
+    print('Hello, {}!'.format(name))
+
+
+def hello(who):
+    print("Welcome to the {}!".format(who))
+
+
+def even_or_not(name):
+    q, e = 0, 0
+    counter = 3
+    while counter > 0:
+        print('Answer "yes" if the number is even, otherwise answer "no"')
+        number = randint(1, 1000)
+        print('Question: {}'.format(number))
+        text = input("Answer: ")
+        if number % 2 == 0 and text == 'yes':
+            print("Correct!")
+            q += 1
+        elif number % 2 != 0 and text == 'yes':
+            print(
+                "'yes' is wrong answer ;(. Correct answer was 'no'. "
+                "\nLet's try again, {}!".format(name))
+            e += 1
+        elif number % 2 == 0 and text == 'no':
+            print(
+                "'no' is wrong answer ;(. Correct answer was 'yes'."
+                "\nLet's try again, {}!".format(name))
+            e += 1
+        elif number % 2 != 0 and text == 'no':
+            print("Correct!")
+            q += 1
+        else:
+            return print(
+                "Answer is wrong ;(. Let's try again,"
+                "{}!".format(name))
+            e += 1
+        counter -= 1
+
+    if q == 3:
+        return print("Congratulations, {}!".format(name))
+    else:
+        return print("You lost, {}!".format(name))
+
+
+def main():
+    hello('Brain Games')
+    name = prompt.string('May I have your name? ')
+    welcome_user(name)
+    even_or_not(name)
+
+
+if __name__ == "__main__":
+    main()
